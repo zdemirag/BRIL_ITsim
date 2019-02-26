6d1565d20a6da72ca0239796eae87ec0cc5a362b
Georg Auzinger <georg.auzinger@cern.ch>
2019-02-26 17:26:41 +0100

    work in progress on linearity.py update

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -3,28 +3,36 @@
 PUIN=$1
 DATAPATH=/eos/user/g/gauzinge/PUdata/
 
-echo "running BRIL IT Cluster analysis"
 eval $(scramv1 runtime -sh) || echo "The command 'cmsenv' failed!"
 
+echo '####################################################################'
+echo "running BRIL IT Cluster analysis"
+echo 'Picking up data files with PU ' ${PUIN} ' from directory: ' ${DATAPATH}
+echo '####################################################################'
+echo 'Processing files:'
 for filename in ${DATAPATH}*.root; do
     #get the Proc ID
-    TAG=$(echo ${filename} | cut -d'.' -f 2)
+    TAG=$(echo ${filename} | cut -d'.' -f 3)
     tmp=$(echo ${filename} | cut -d'_' -f 4)
-    PU=$(echo ${tmp} | cut -d'.' -f 1)
-    echo ${TAG} ${PU}
-    if [[ "$PUIN" -eq ${PU} ]]; then
+    MAIN=$(echo ${tmp} | cut -d'.' -f 1)
+    DECIMAL=$(echo ${filename} | cut -d'.' -f 2)
+    PU=${MAIN}.${DECIMAL}
+    #if [[ "$PUIN" -eq "$PU" ]]; then
+    if [ "$PUIN" = "$PU" ]; then
+    echo ${filename} 'with PU ' ${PU}
+    #if (( $(echo "$PUIN = $PU" | bc -l) )); then
         cmsRun ITclusterAnalyzer/python/ITclusterAnalyzer_cfg.py print \
             inputFiles=file:${filename} \
-            outputFile=summary.root \
+            outputFile=temp.root \
             tag=${TAG}
-    else
-        echo "Processing for PU ${PUIN} requested but this file: ${filename} has PU ${PU} in the name!"
+    #else
+        #echo "Processing for PU ${PUIN} requested but this file: ${filename} has PU ${PU} in the name!"
     fi
 done
 
 echo "Now merging output histograms"
-command="hadd summary_PU${PUIN}.root"
-for rootfile in ${PWD}/*.root; do
+command="hadd summary_PU_${PUIN}.root"
+for rootfile in ${PWD}/temp_?.root; do
     command+=" ${rootfile}"
 done
 echo $command
