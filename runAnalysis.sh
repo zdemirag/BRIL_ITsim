7dac5b59c20b1a8820388f8f96072c935dd117ff
Cristina Oropeza Barrera <cbarrera@cern.ch>
2019-03-11 21:01:03 +0100

    Fixing script to correctly identify PU value, including those with decimal point.

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -8,17 +8,29 @@ eval $(scramv1 runtime -sh) || echo "The command 'cmsenv' failed!"
 
 for filename in ${DATAPATH}*.root; do
     #get the Proc ID
+    hasDec=$(echo $PUIN | grep ".")
+    PUINint=$(echo $PUIN | cut -d'.' -f 1)
+    #echo $PUIN
+    if [[ `expr index "$PUIN" \.` -gt 0 ]]; then
+        PUINdec=$(echo $PUIN | cut -d'.' -f 2)
+    else 
+        PUINdec=0
+    fi
+    #echo `expr index "$PUIN" \.`
+    #echo ${PUINint} ${PUINdec}
     TAG=$(echo ${filename} | cut -d'.' -f 2)
     tmp=$(echo ${filename} | cut -d'_' -f 4)
     PU=$(echo ${tmp} | cut -d'.' -f 1)
-    echo ${TAG} ${PU}
-    if [[ "$PUIN" -eq ${PU} ]]; then
+    dec=$(echo ${tmp} | cut -d'.' -f 2)
+    #echo ${PU} ${dec}
+    if [[ ${PUINint} -eq ${PU} && ${PUINdec} -eq ${dec} ]]; then
+        echo "Processing for PU ${PUIN}. File: ${filename} matches the criteria!"
         cmsRun ITclusterAnalyzer/python/ITclusterAnalyzer_cfg.py print \
             inputFiles=file:${filename} \
             outputFile=summary.root \
             tag=${TAG}
     else
-        echo "Processing for PU ${PUIN} requested but this file: ${filename} has PU ${PU} in the name!"
+        echo "Processing for PU ${PUIN}. Skipping file: ${filename}."
     fi
 done
 
